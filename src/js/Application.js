7b6e982547886d7d3b7ce148cbb44d19972c8c58
Gabriela Hristova <gabriela.hr1406@gmail.com>
2023-01-17 00:03:05 +0200

    add

--- src/js/Application.js
+++ src/js/Application.js
@@ -20,7 +20,7 @@ export default class Application extends EventEmitter {
   }
 
   _create() {
-    const lyrics = ['Ah', 'ha', 'ha', 'ha', 'stayin alive', 'stayin alive'];
+    const lyrics = ['Ah', 'ha', 'ha', 'ha', `stayin' alive`, `stayin' alive`];
     let count = 0;
     this._beat.addListener(Beat.events.BIT, (bit) => {
       const message = document.createElement('div');
